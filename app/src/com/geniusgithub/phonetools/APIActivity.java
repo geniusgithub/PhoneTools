672fd8b6eb81852102d814858c75f00a15f314e3
geniusgithub <lance.wyj@gmail.com>
2016-03-07 15:00:41 +0800

    update ui

--- app/src/com/geniusgithub/phonetools/APIActivity.java
+++ app/src/com/geniusgithub/phonetools/APIActivity.java
@@ -3,18 +3,20 @@ package com.geniusgithub.phonetools;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.geniusgithub.calllog.CallLogFragment;
 import com.geniusgithub.phonetools.util.CommonLog;
@@ -34,7 +36,7 @@ public class APIActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_api_layout);
+        setContentView(R.layout.main_layout);
 
         initView();
     }
@@ -49,10 +51,10 @@ public class APIActivity extends AppCompatActivity {
          ab.setDisplayHomeAsUpEnabled(true);
 
          mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_main_drawer);
-//         NavigationView navigationView = (NavigationView) findViewById(R.id.nv_main_navigation);
-//         if (navigationView != null) {
-//             setupDrawerContent(navigationView);
-//         }
+         NavigationView navigationView = (NavigationView) findViewById(R.id.nv_main_navigation);
+         if (navigationView != null) {
+             setupDrawerContent(navigationView);
+         }
 
 //         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //         fab.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +65,7 @@ public class APIActivity extends AppCompatActivity {
 //                             @Override
 //                             public void onClick(View v) {
 //                                 Toast.makeText(
-//                                         MainActivity.this,
+//                                         APIActivity.this,
 //                                         "Toast comes out",
 //                                         Toast.LENGTH_SHORT).show();
 //                             }
--- app/src/com/geniusgithub/phonetools/TestActivity.java
+++ app/src/com/geniusgithub/phonetools/TestActivity.java
@@ -2,23 +2,31 @@ package com.geniusgithub.phonetools;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class TestActivity extends Activity{
+public class TestActivity extends AppCompatActivity{
 
 	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ui_layout);
+        setContentView(R.layout.test_layout);
 
         initView();
     }
 
     
     private void initView(){
+   	 Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      setSupportActionBar(toolbar);
 
+      final ActionBar ab = getSupportActionBar();
+      ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+      ab.setDisplayHomeAsUpEnabled(true);
 
     }
 }
